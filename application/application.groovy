9f66ae3ebb36da9734161d0fc1bbefd32be1c9ca
Roman Reva <roman.reva@proposalsfactory.com>
2012-10-17 20:30:07 +0200

    HOTFIX created

--- application/application.groovy
+++ application/application.groovy
@@ -1,9 +1,9 @@
 println "This is mega super demo application"
 
 def a = 10
-def b = 20
+def b = 30
 
-def result = a + b * 31
+def result = a + b * 41
 println "Result: ${result}"
 
 println "The end!"
