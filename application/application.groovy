112b64c59eeaf9806994d5e8f86120868af1b543
Roman Reva <roman.reva@proposalsfactory.com>
2012-10-17 20:12:55 +0200

    Feature S101 was implemented

--- application/application.groovy
+++ application/application.groovy
@@ -2,8 +2,9 @@ println "This is mega super demo application"
 
 def a = 10
 def b = 20
+def coef = 0.12
 
-def result = a + b * 31
+def result = (a + b * 31) * coef
 println "Result: ${result}"
 
 println "The end!"
