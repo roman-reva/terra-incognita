7e48761dbfd4a2e3433c9ee5c98ed18f3412c718
Roman Reva <roman.reva@proposalsfactory.com>
2012-11-13 11:24:11 +0100

    DEMO CLEANUP

--- application/application.groovy
+++ application/application.groovy
@@ -2,12 +2,7 @@ println "This is mega super demo application"
 
 def a = 10
 def b = 30
-def coef = 0.12
 
-def result = (a + b * 41) * coef // this is hotfix upd!
-println "Result: ${result}"
+def result = a + b
 
-def constant = 202
-println "And constant equals $constant!!!"
-
-println "The end!"
+println "Result = ${result}"
